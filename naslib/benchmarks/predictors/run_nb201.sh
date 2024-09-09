74798f7ca09cb316f6619cd7c0e1c233cf8b3814
Timur Carstensen <timurcarstensen@gmail.com>
2024-09-09 15:56:02 +0200

    feat: using slurm scheduler for execution in nas predictor eval

--- naslib/benchmarks/predictors/run_nb201.sh
+++ naslib/benchmarks/predictors/run_nb201.sh
@@ -1,22 +1,20 @@
-# predictors=(fisher grad_norm grasp jacov snip synflow \
-# lce lce_m sotl sotle valacc valloss \
-# lcsvr omni_ngb omni_seminas \
-# bananas bonas gcn mlp nao seminas \
-# lgb ngb rf xgb \
-# bayes_lin_reg bohamiann dngo \
-# gp sparse_gp var_sparse_gp)
+predictors=(fisher grad_norm grasp jacov snip synflow \
+lce lce_m sotl sotle valacc valloss \
+lcsvr omni_ngb omni_seminas \
+bananas bonas gcn mlp nao seminas \
+lgb ngb rf xgb \
+bayes_lin_reg bohamiann dngo \
+gp sparse_gp var_sparse_gp tabpfn)
 
-predictors=(tabpfn xgb)
+experiment_types=(single single single single single single \
+vary_fidelity vary_fidelity vary_fidelity vary_fidelity vary_fidelity vary_fidelity \
+vary_both vary_both vary_both \
+vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size \
+vary_train_size vary_train_size vary_train_size vary_train_size \
+vary_train_size vary_train_size vary_train_size \
+vary_train_size vary_train_size vary_train_size vary_train_size) 
 
-# experiment_types=(single single single single single single \
-# vary_fidelity vary_fidelity vary_fidelity vary_fidelity vary_fidelity vary_fidelity \
-# vary_both vary_both vary_both \
-# vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size vary_train_size \
-# vary_train_size vary_train_size vary_train_size vary_train_size \
-# vary_train_size vary_train_size vary_train_size \
-# vary_train_size vary_train_size vary_train_size)
-
-experiment_types=(vary_train_size vary_train_size)
+# experiment_types=(vary_train_size vary_train_size)
 
 start_seed=$1
 if [ -z "$start_seed" ]; then
@@ -40,7 +38,32 @@ test_size=200
 
 echo "Creating configs..."
 
-# create config files
+# # create config files
+# for i in $(seq 0 $((${#predictors[@]} - 1))); do
+#     predictor=${predictors[$i]}
+#     experiment_type=${experiment_types[$i]}
+#     python $base_file/benchmarks/create_configs.py --predictor $predictor --experiment_type $experiment_type \
+#         --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
+#         --dataset=$dataset --config_type predictor --search_space $search_space
+# done
+
+# echo "created configs"
+
+# run experiments
+# for t in $(seq $start_seed $end_seed); do
+#     for predictor in ${predictors[@]}; do
+#         config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
+#         echo ================running $predictor trial: $t =====================
+#         python $base_file/benchmarks/predictors/runner.py --config-file $config_file
+#     done
+#     if [ "$save_to_s3" ]; then
+#         # zip and save to s3
+#         echo zipping and saving to s3
+#         zip -r $out_dir.zip $out_dir
+#         python $base_file/benchmarks/upload_to_s3.py --out_dir $out_dir --s3_folder $s3_folder
+#     fi
+# done
+
 for i in $(seq 0 $((${#predictors[@]} - 1))); do
     predictor=${predictors[$i]}
     experiment_type=${experiment_types[$i]}
@@ -51,17 +74,13 @@ done
 
 echo "created configs"
 
-# run experiments
+# submit jobs to scheduler
 for t in $(seq $start_seed $end_seed); do
     for predictor in ${predictors[@]}; do
         config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
-        echo ================running $predictor trial: $t =====================
-        python $base_file/benchmarks/predictors/runner.py --config-file $config_file
+        echo "Submitting job for $predictor trial: $t"
+        sbatch $base_file/benchmarks/scheduler.sh $config_file $predictor $t
     done
-    if [ "$save_to_s3" ]; then
-        # zip and save to s3
-        echo zipping and saving to s3
-        zip -r $out_dir.zip $out_dir
-        python $base_file/benchmarks/upload_to_s3.py --out_dir $out_dir --s3_folder $s3_folder
-    fi
 done
+
+echo "All jobs submitted to scheduler"
--- naslib/benchmarks/scheduler.sh
+++ naslib/benchmarks/scheduler.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-#SBATCH -p alldlc_gpu-rtx2080 #gpu_tesla-P100     #ml_gpu-rtx2080     # bosch_gpu-rtx2080    #alldlc_gpu-rtx2080     # partition (queue)
-#SBATCH --mem 4000            # memory pool for all cores (4GB)
-#SBATCH -t 10-00:00           # time (D-HH:MM)
-#SBATCH -c 1                  # number of cores
+#SBATCH -p alldlc_gpu-rtx2080
+#SBATCH --mem 16000            # memory pool for all cores (4GB)
+#SBATCH -t 00-10:00           # time (D-HH:MM)
+#SBATCH -c 8                # number of cores
 #SBATCH --gres=gpu:1          # reserves one GPU
 #SBATCH -o %x.%A.%N.out       # STDOUT  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
 #SBATCH -e %x.%A.%N.err       # STDERR  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
 #SBATCH --mail-type=END,FAIL  # (recive mails about end and timeouts/crashes of your job)
-#SBATCH -J hiera              # sets the job name. 
+#SBATCH -J naslib-predictor-eval              # sets the job name.
 
 # Print some information about the job to STDOUT
 #set -x
 
-echo "Workingdir: $PWD";
-echo "Started at $(date)";
-echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
-
-# Activate virtual env so that run_experiment can load the correct packages
-source /home/ruchtem/dev/venvs/naslib/bin/activate
-
-
-if [ -z "$3" ]; then
-    COMMAND="python -u runner.py --config-file $1 optimizer $2";
-else
-    COMMAND="python -u runner.py --config-file $1 optimizer $2 seed $3";
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <config_file> <predictor> <trial>"
+    exit 1
 fi
 
-echo $COMMAND;
-eval $COMMAND;
+config_file=$1
+predictor=$2
+trial=$3
+
+base_file=/work/dlclarge1/carstent-timur_thesis_follow_up/NASLib
+
+echo "Config file: $config_file"
+echo "Predictor: $predictor"
+echo "Trial: $trial"
+echo "Workingdir: $PWD"
+echo "Started at $(date)"
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION"
+
+# Activate virtual env so that run_experiment can load the correct packages
+source /work/dlclarge1/carstent-timur_thesis_follow_up/NASLib/.venv/bin/activate
+
+python $base_file/benchmarks/predictors/runner.py --config-file $config_file
 
 # Print some Information about the end-time to STDOUT
-echo "DONE";
-echo "Finished at $(date)";
+echo "DONE"
+echo "Finished at $(date)"
