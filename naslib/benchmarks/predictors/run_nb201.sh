8743ff2b944a36724c7a591257241c2571014383
Timur Carstensen <timurcarstensen@gmail.com>
2024-09-16 15:54:46 +0200

    feat: made sbatch submission script work with array jobs

--- naslib/benchmarks/predictors/run_nb201.sh
+++ naslib/benchmarks/predictors/run_nb201.sh
@@ -1,70 +1,70 @@
 predictors=(
-    fisher
-    grad_norm
-    grasp
-    jacov
-    snip
-    synflow
-    lce
-    lce_m
-    sotl
-    sotle
-    valacc
-    valloss
-    lcsvr
-    omni_ngb
-    omni_seminas
-    bananas
-    bonas
-    gcn
-    mlp
-    nao
-    seminas
-    lgb
-    ngb
-    rf
-    xgb
-    bayes_lin_reg
-    bohamiann
-    dngo
-    gp
-    sparse_gp
-    var_sparse_gp
+    # fisher TODO: ditch zcps
+    # grad_norm TODO: ditch multi-fidelity
+    # grasp
+    # jacov
+    # snip
+    # synflow
+    # lce
+    # lce_m
+    # sotl
+    # sotle
+    # valacc
+    # valloss
+    # lcsvr
+    # omni_ngb
+    # omni_seminas
+    # bananas
+    # bonas
+    # gcn
+    # mlp
+    # nao
+    # seminas
+    # lgb
+    # ngb
+    # rf
+    # xgb
+    # bayes_lin_reg
+    # bohamiann
+    # dngo
+    # gp
+    # sparse_gp
+    # var_sparse_gp
     tabpfn
 )
 
 experiment_types=(
-    single
-    single
-    single
-    single
-    single
-    single
-    vary_fidelity
-    vary_fidelity
-    vary_fidelity
-    vary_fidelity
-    vary_fidelity
-    vary_fidelity
-    vary_both
-    vary_both
-    vary_both
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
-    vary_train_size
+    # single
+    # single
+    # single
+    # single
+    # single
+    # single
+    # vary_fidelity
+    # vary_fidelity
+    # vary_fidelity
+    # vary_fidelity
+    # vary_fidelity
+    # vary_fidelity
+    # vary_both
+    # vary_both
+    # vary_both
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
+    # vary_train_size
     vary_train_size
 )
 
@@ -80,59 +80,46 @@ out_dir=$s3_folder\_$start_seed
 
 # search space / data:
 search_space=nasbench201
-dataset=cifar10
+datasets=(cifar10 cifar100 ImageNet16-120)
 
 # other variables:
 trials=5
 end_seed=$(($start_seed + $trials - 1))
-save_to_s3=false
-test_size=200
+test_size=1000
 
 echo "Creating configs..."
 
-# # create config files
-# for i in $(seq 0 $((${#predictors[@]} - 1))); do
-#     predictor=${predictors[$i]}
-#     experiment_type=${experiment_types[$i]}
-#     python $base_file/benchmarks/create_configs.py --predictor $predictor --experiment_type $experiment_type \
-#         --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
-#         --dataset=$dataset --config_type predictor --search_space $search_space
-# done
-
-# echo "created configs"
-
-# run experiments
-# for t in $(seq $start_seed $end_seed); do
-#     for predictor in ${predictors[@]}; do
-#         config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
-#         echo ================running $predictor trial: $t =====================
-#         python $base_file/benchmarks/predictors/runner.py --config-file $config_file
-#     done
-#     if [ "$save_to_s3" ]; then
-#         # zip and save to s3
-#         echo zipping and saving to s3
-#         zip -r $out_dir.zip $out_dir
-#         python $base_file/benchmarks/upload_to_s3.py --out_dir $out_dir --s3_folder $s3_folder
-#     fi
-# done
-
-for i in $(seq 0 $((${#predictors[@]} - 1))); do
-    predictor=${predictors[$i]}
-    experiment_type=${experiment_types[$i]}
-    python $base_file/benchmarks/create_configs.py --predictor $predictor --experiment_type $experiment_type \
-        --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
-        --dataset=$dataset --config_type predictor --search_space $search_space
+for dataset in "${datasets[@]}"; do
+    for i in $(seq 0 $((${#predictors[@]} - 1))); do
+        predictor=${predictors[$i]}
+        experiment_type=${experiment_types[$i]}
+        python $base_file/benchmarks/create_configs.py --predictor $predictor --experiment_type $experiment_type \
+            --test_size $test_size --start_seed $start_seed --trials $trials --out_dir $out_dir \
+            --dataset $dataset --config_type predictor --search_space $search_space --fidelity_single 199
+    done
 done
 
 echo "created configs"
 
-# submit jobs to scheduler
-for t in $(seq $start_seed $end_seed); do
-    for predictor in ${predictors[@]}; do
-        config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
-        echo "Submitting job for $predictor trial: $t"
-        sbatch $base_file/benchmarks/scheduler.sh $config_file $predictor $t
+# Create a job array file
+job_array_file="job_array_${start_seed}.txt"
+> $job_array_file
+
+# Populate the job array file
+for dataset in "${datasets[@]}"; do
+    for t in $(seq $start_seed $end_seed); do
+        for i in $(seq 0 $((${#predictors[@]} - 1))); do
+            predictor=${predictors[$i]}
+            config_file=$out_dir/$dataset/configs/predictors/config\_$predictor\_$t.yaml
+            echo "$config_file $predictor $t $dataset" >> $job_array_file
+        done
     done
 done
 
-echo "All jobs submitted to scheduler"
+# Count total jobs
+total_jobs=$(wc -l < $job_array_file)
+
+# Submit the array job
+sbatch --array=1-$total_jobs $base_file/benchmarks/scheduler.sh $job_array_file
+
+echo "Array job submitted to scheduler"
--- naslib/benchmarks/scheduler.sh
+++ naslib/benchmarks/scheduler.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 #SBATCH -p alldlc_gpu-rtx2080
-#SBATCH --mem 16000            # memory pool for all cores (4GB)
+#SBATCH --mem 32000            # memory pool for all cores (4GB)
 #SBATCH -t 00-10:00           # time (D-HH:MM)
 #SBATCH -c 8                # number of cores
 #SBATCH --gres=gpu:1          # reserves one GPU
-#SBATCH -o %x.%A.%N.out       # STDOUT  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
-#SBATCH -e %x.%A.%N.err       # STDERR  (the folder log has to exist) %A will be replaced by the SLURM_ARRAY_JOB_ID value
-#SBATCH --mail-type=END,FAIL  # (recive mails about end and timeouts/crashes of your job)
+#SBATCH -o slurm_logs/%x.%A.%a.out       # STDOUT  (the folder log has to exist) %A is the job ID and %a is the array index
+#SBATCH -e slurm_logs/%x.%A.%a.err       # STDERR  (the folder log has to exist) %A is the job ID and %a is the array index
+#SBATCH --mail-type=END,FAIL  # (receive mails about end and timeouts/crashes of your job)
 #SBATCH -J naslib-predictor-eval              # sets the job name.
+#SBATCH --exclude=dlcgpu04,dlcgpu35,dlcgpu39  # exclude specific nodes
 
-# Print some information about the job to STDOUT
-#set -x
+# Get the job parameters from the job array file
+job_array_file=$1
+job_params=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $job_array_file)
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <config_file> <predictor> <trial>"
-    exit 1
-fi
-
-config_file=$1
-predictor=$2
-trial=$3
+# Parse job parameters
+read -r config_file predictor trial dataset <<< "$job_params"
 
 base_file=/work/dlclarge1/carstent-timur_thesis_follow_up/NASLib/naslib
 
 echo "Config file: $config_file"
 echo "Predictor: $predictor"
 echo "Trial: $trial"
+echo "Dataset: $dataset"
 echo "Workingdir: $PWD"
 echo "Started at $(date)"
 echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION"
